72074312ae6efce6b8629b78a7fccc4c644f1c5d
Lars Janssen <janssen.lars@gmail.com>
2022-03-16 15:26:54 +0100

    pre-commit test 7

--- run_batches.py
+++ run_batches.py
@@ -1,11 +1,15 @@
-from matplotlib import pyplot as plt
+from __future__ import annotations
+
 import multiprocessing as mp
-from tqdm import tqdm
-import batch
+
 import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+import batch
 
 # Amount of times the batch_size is doubled and e is halved.
-doubles = 2
+doubles = 4
 
 # The amount of rounds in each game.
 batch_size = 1000 * (2 ** doubles)
@@ -25,7 +29,7 @@ p2 = [0, 0, 0, 1]
 
 # The probability that player 2 makes a move different to its strategy.
 e = 0.1 / (2 ** doubles)
-d=0
+d = 0
 
 # Interval of deltas in which we find the best counter-strategy.
 start = 0
@@ -34,10 +38,12 @@ end = 100
 # How many (equally spaced) points we check in the interval.
 steps = 10
 
+
 def dectobin(number):
     """Converts a decimal number to a list of binary numbers of length 4."""
     output = [int(x) for x in bin(number)[2:]]
     return [0] * (4 - len(output)) + output
+
 
 def test_delta(delta):
     """Function to run in parallel."""
@@ -45,19 +51,27 @@ def test_delta(delta):
     batch1.run()
     return batch1.return_values()
 
+
 class counterstrats:
     def __init__(self):
         # Make a list of the deltas to check.
-        self.deltas = [(start + (end - start) * i / steps) / 100 for i in range(steps)]
+        self.deltas = [(start + (end - start) * i / steps) /
+                       100 for i in range(steps)]
         self.new_strat1 = [0, 0, 0, 0]
-        # Make a list where we put how many times each strat is chosen as the new one.
+        # Make a list where we put how many times each strat is chosen
+        # as the new one.
         self.new_strat = np.zeros((len(self.deltas), 16))
 
     def get_data(self):
         for i in tqdm(range(len(self.deltas))):
             # Simultaneously run the batches.
             pool = mp.Pool(mp.cpu_count())
-            result = pool.map(test_delta, [self.deltas[i] for _ in range(iterations)])
+            result = pool.map(
+                test_delta, [
+                    self.deltas[i]
+                    for _ in range(iterations)
+                ],
+            )
             pool.close()
 
             # Add the results to new_strat.
@@ -78,14 +92,15 @@ class counterstrats:
 
         # Make the plot.
         plt.plot(self.deltas, G)
-        plt.legend(["[0,0,0,0]"])
+        plt.legend(['[0,0,0,0]'])
         plt.ylim(0, iterations + iterations / 10)
-        plt.axvline(x=1 / 3, ymin=0, ymax=iterations, color="orange")
+        plt.axvline(x=1 / 3, ymin=0, ymax=iterations, color='orange')
         plt.title(str(batch_size))
         # plt.show()
-        plt.savefig("Approx/" + str(batch_size) + ".png")
+        plt.savefig('Approx/' + str(batch_size) + '.png')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     counterstat = counterstrats()
     counterstat.get_data()
     counterstat.print_and_plot()
